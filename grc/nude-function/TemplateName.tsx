57cac6260e93f63a111b5e5b1355a188fae1828d
champix56 <desorbaix@free.fr>
2025-12-15 23:31:12 +0100

    add page type

---
+++ grc/nude-function/TemplateName.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TemplateName: React.FC = () => {
+  return (
+    <div data-testid="TemplateName">
+      page templateName
+    </div>
+  );
+}
+export default TemplateName;
---
+++ grc/nude-function/readme.md
@@ -0,0 +1,5 @@
+# page TemplateName
+
+## usage de page templateName
+
+&lt;TemplateName /&gt;
