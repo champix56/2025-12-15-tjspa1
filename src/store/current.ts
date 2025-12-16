a3ac2d6fc64c14f58b563737d704731eac0f3b5a
champix56 <desorbaix@free.fr>
2025-12-16 20:37:51 +0100

    tranche current

---
+++ src/store/current.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { emptyMeme, type MemeInterface } from 'orsys-tjs-meme';
+interface ICurrentState {
+    meme: MemeInterface
+}
+const initialState: ICurrentState = {
+    meme: emptyMeme
+}
+
+const current = createSlice({
+    name: 'current',
+    initialState,
+    reducers: {
+        update: (state, { payload }: { type: string, payload: MemeInterface }) => {
+            state.meme = payload
+        },
+        clear: (state) => {
+            state.meme = emptyMeme
+        }
+    }
+});
+
+export const { update, clear } = current.actions
+
+const currentReducer = current.reducer
+export default currentReducer
