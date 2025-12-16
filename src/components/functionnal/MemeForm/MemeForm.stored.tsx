d763321415d4fd24dc4aab08d1a017baa10f184e
champix56 <desorbaix@free.fr>
2025-12-16 21:10:18 +0100

    injection de fonctions

--- src/components/functionnal/MemeForm/MemeForm.stored.tsx
+++ src/components/functionnal/MemeForm/MemeForm.stored.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react'
 import type { IStoredMemeFormProps } from './MemeForm.interface'
 import StandaloneMemeForm from './MemeForm'
 import { store } from '../../../store/store'
+import { saveCurrent } from '../../../store/asyncCaller'
 import { emptyMeme, type ImageInterface, type MemeInterface } from 'orsys-tjs-meme'
+import { update } from '../../../store/current'
 
 const MemeForm: React.FC<IStoredMemeFormProps> = (props) => {
     const [images, setimages] = useState<Array<ImageInterface>>([])
@@ -15,8 +17,11 @@ const MemeForm: React.FC<IStoredMemeFormProps> = (props) => {
             setcurrent(store.getState().current.meme)
         })
     }, [])
-
-    return <StandaloneMemeForm {...props} images={images} meme={current} />
+    return <StandaloneMemeForm {...props} images={images} meme={current} onMemeSave={(meme) => {
+        store.dispatch(saveCurrent(meme));
+    }} onMemeChange={(meme) => {
+        store.dispatch(update(meme))
+    }} />
 }
 
-export default MemeForm.stored
+export default MemeForm
