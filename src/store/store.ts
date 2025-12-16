0eafbde6c7dc46abcdacbc1387486514c8c1e30f
champix56 <desorbaix@free.fr>
2025-12-16 20:33:34 +0100

    typage ts du store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ressourcesReducer, { addMeme, removeMeme } from "./ressources";
+import ressourcesReducer from "./ressources";
 import { loadRessources } from "./asyncCaller";
 
 const store = configureStore({ reducer: { ressources: ressourcesReducer } });
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(loadRessources());
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
