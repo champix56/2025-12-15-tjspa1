6cc1864ff3432de822ebd34f9c8414a4d1fd0ef9
champix56 <desorbaix@free.fr>
2025-12-16 20:05:00 +0100

    avec constructeur d'action

--- src/store/store.js
+++ src/store/store.js
@@ -9,20 +9,28 @@ const reducer = (state = initialState, { type, payload }) => {
     case "REMOVE_MEME":
       const position = state.memes.findIndex(m => m.id === payload.id)
       if (position < 0) return state;
-      return { ...state, memes: [...state.memes.slice(0, position, ...state.memes.slice(position + 1)] }
+      return { ...state, memes: [...state.memes.slice(0, position), ...state.memes.slice(position + 1)] }
     default:
       return state
   }
 }
+
+const addMeme = (meme) => {
+  return { type: 'ADD_MEME', payload: meme }
+}
+const removeMeme = (meme) => {
+  return { type: 'REMOVE_MEME', payload: meme }
+}
+
 let state = reducer(undefined, { type: '@@INIT' });
 console.log(state);
-state = reducer(state, { type: 'ADD_MEME', payload: { id: 0, name: 'coucou' } });
+state = reducer(state, addMeme({ id: 0, name: 'coucou' }));
 console.log(state);
-state = reducer(state, { type: 'ADD_MEME', payload: { id: 1, name: 'toto' } });
+state = reducer(state, addMeme({ id: 1, name: 'toto' }));
 console.log(state);
-state = reducer(state, { type: 'ADD_MEME', payload: { id: 2, name: 'hello' } });
+state = reducer(state, addMeme({ id: 2, name: 'hello' }));
 console.log(state);
-state = reducer(state, { type: 'ADD_MEME', payload: { id: 3, name: 'ola' } });
+state = reducer(state, addMeme({ id: 3, name: 'ola' }));
 console.log(state);
-state = reducer(state, { type: 'REMOVE_MEME', payload: { id: 1, name: 'toto' } });
+state = reducer(state, removeMeme({ id: 1, name: 'toto' }));
 console.log(state);
