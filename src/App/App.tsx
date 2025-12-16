2a6177b5358d70a1a77d7d4f8e773c4d45cfbe85
champix56 <desorbaix@free.fr>
2025-12-16 21:21:26 +0100

    full connected components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,24 +1,13 @@
-import { emptyMeme, MemeSVGViewer, type ImageInterface } from "orsys-tjs-meme";
 import MemeForm from "../components/functionnal/MemeForm/MemeForm.stored.js";
 import FlexH3Grow from "../components/layout/FlexH3Grow/FlexH3Grow";
 import FlexV1Grow from "../components/layout/FlexV1Grow/FlexV1Grow";
 import Footer from "../components/ui/Footer/Footer";
 import Header from "../components/ui/Header/Header";
 import Navbar from "../components/ui/Navbar/Navbar";
-import { useEffect, useState } from "react";
-import { REST_API_RESSOURCES, REST_API_URL } from "../config/constantes";
-import '../store/store.js'
-type TImages = Array<ImageInterface>;
+import MemeSvgViewer from "../components/ui/MemeSvgViewer/MemeSvgViewer.stored.js";
+
 
 const App: React.FC = () => {
-  const [current, setcurrent] = useState(emptyMeme);
-  const [images, setImages] = useState<TImages>([]);
-  useEffect(() => {
-    fetch(`${REST_API_URL}${REST_API_RESSOURCES.images}`)
-      .then((r) => r.json())
-      .then((imgs) => setImages(imgs));
-  }, []);
-
   return (
     <>
       {/* <div>images:{JSON.stringify(images)}</div> */}
@@ -27,11 +16,7 @@ const App: React.FC = () => {
           <Header />
           <Navbar />
           <FlexV1Grow>
-            <MemeSVGViewer
-              meme={current}
-              image={images.find((item) => item.id === current.imageId)}
-              basePath=""
-            />
+            <MemeSvgViewer basePath=""/>
             <MemeForm />
           </FlexV1Grow>
           <Footer />
