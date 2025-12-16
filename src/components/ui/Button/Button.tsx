f5e2a1f5ae300bace649098f8af7f5947b3397ea
champix56 <desorbaix@free.fr>
2025-12-16 10:00:14 +0100

    fin button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Button.module.css";
 interface IButtonProps {
-  children: string|React.ReactElement|Array<string|React.ReactElement>;
-  onButtonClick?: ()=>void;
+  children: string | React.ReactElement | Array<string | React.ReactElement>;
+  onButtonClick?: () => void;
   bgColor?: string;
   type?: "button" | "submit" | "reset";
 }
-const Button = ({ children, onButtonClick, bgColor = "red", type="button" }: IButtonProps) => {
-const [isClicked, setisClicked] = useState(false)
+const Button = ({
+  children,
+  onButtonClick,
+  bgColor = "red",
+  type = "button",
+}: IButtonProps) => {
+  const [isClicked, setisClicked] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setisClicked(false);
+    }, 200);
+  }, [isClicked]);
+
   return (
     <button
       type={type}
-      className={style.Button}
+      className={style.Button + (isClicked ? " " + style.clicked : "")}
       style={{ backgroundColor: bgColor }}
       onClick={() => {
         setisClicked(true);
@@ -21,7 +32,8 @@ const [isClicked, setisClicked] = useState(false)
         }
       }}
     >
-      {children}<br/>
+      {children}
+      <br />
       {isClicked.toString()}
     </button>
   );
