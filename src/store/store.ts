540164ce55ec3e0da0039d3e2d09c133732a82bc
champix56 <desorbaix@free.fr>
2025-12-17 13:49:58 +0100

    appel de chargement initial

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ressourcesReducer from "./ressources";
 import currentReducer from "./current";
+import { fetchDatas } from "./asyncCaller";
 
 export const store = configureStore({
-  reducer: { ressources: ressourcesReducer, current:currentReducer },
+  reducer: { ressources: ressourcesReducer, current: currentReducer },
 });
 
 //type state et dispatch
 export type StoreState = ReturnType<typeof store.getState>;
 export type StoreDispatch = typeof store.dispatch;
-
+store.dispatch(fetchDatas());
 
 store.subscribe(() => {
   console.log(store.getState());
